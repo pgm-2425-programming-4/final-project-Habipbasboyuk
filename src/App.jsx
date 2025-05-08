f4c5ba009f9b12c43e9c26f5c6f5e91eec6dac84
basboyukhabip <habibasb@student.arteveldehs.be>
2025-05-08 23:38:35 +0200

    initial commit and already added some jsx and some styling

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <section className="task-container">
+      <div>
+        <p>To Do's</p>
+
+        <article className="task-card">
+          <button className="task-card__button btn">Development</button>
+
+          <p className="task-card__name">Code something semantic</p>
+          <p className="task-card__date">19/05/2006</p>
+        </article>
+      </div>
+
+      <div>
+        <p>In progress</p>
+
+        <article className="task-card">
+          <button className="task-card__button btn">Development</button>
+
+          <p className="task-card__name">Code something semantic</p>
+          <p className="task-card__date">19/05/2006</p>
+        </article>
+      </div>
+
+      <div>
+
+
+        <p>Done</p>
+
+        <article className="task-card">
+
+
+          <button className="task-card__button btn ">Development</button>
+
+          <p className="task-card__name">Code something semantic</p>
+          <p className="task-card__date">19/05/2006</p>
+
+
+        </article>
+
+
+      </div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function header() {
+
+        return (
+          <header>
+      
+          <nav>
+            <div>
+              <h1>Dashboard</h1>
+              <p>You habe <span className='highlight'>5 Tasks!</span></p>
+            </div>
+      
+            <div>
+              <button>
+                PGM-3
+              </button>
+      
+              <button>
+                PGM-4
+              </button>
+            </div>
+          </nav>
+      
+          </header>
+      
+      
+      
+        );
+      }
+
