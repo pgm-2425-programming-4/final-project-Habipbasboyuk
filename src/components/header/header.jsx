e712d0600219911b7938868e0b946cae613352d7
basboyukhabip <habibasb@student.arteveldehs.be>
2025-05-12 22:16:31 +0200

    added html strucure, and the design of the header and a little bit of the main

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 export default function header() {
 
         return (
-          <header>
+          <header className='header'>
       
-          <nav>
-            <div>
-              <h1>Dashboard</h1>
-              <p>You habe <span className='highlight'>5 Tasks!</span></p>
+          <nav className='navbar'>
+            <div className='heading'>
+              <h1 className='heading__title'>Dashboard</h1>
+              <h2 className='heading__status'>You have <span className='highlight'>5 Tasks!</span></h2>
             </div>
       
-            <div>
-              <button>
+            <div className='project-container'>
+              <button className='btn btn-grey project-container__button'>
                 PGM-3
               </button>
       
-              <button>
+              <button className='btn btn-grey project-container__button'>
                 PGM-4
               </button>
             </div>
