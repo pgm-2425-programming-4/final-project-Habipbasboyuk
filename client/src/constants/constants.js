04bb57404be031e7e924e27e2e8a0857d7428669
basboyukhabip <habibasb@student.arteveldehs.be>
2025-05-22 11:19:42 +0200

    added the good api url

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,6 +1,6 @@
 	
 export const API_URL = import.meta.env.PROD
-  ? "https://final-project-habipbasboyuk.onrender.com"
+  ? "https://final-project-habipbasboyuk.onrender.com/api/todos"
   : "http://localhost:1337/api/todos";
   
 export const API_TOKEN = import.meta.env.PROD
